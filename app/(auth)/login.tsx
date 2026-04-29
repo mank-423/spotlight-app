e23f1c955d7f1ddc78b674009a8f8f82152e3874
Mayank Kumar <mk4664348@gmail.com>
2026-04-30 00:13:10 +0530

    Login page

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,26 +1,26 @@
 import { COLORS } from "@/constants/theme";
 import { styles } from "@/styles/auth.styles";
-import { useSSO } from "@clerk/clerk-expo";
+// import { useSSO } from "@clerk/clerk-expo";
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { View, Text, Image, TouchableOpacity } from "react-native";
 
 export default function login() {
-  const { startSSOFlow } = useSSO();
+  // const { startSSOFlow } = useSSO();
   const router = useRouter();
 
-  const handleGoogleSignIn = async () => {
-    try {
-      const { createdSessionId, setActive } = await startSSOFlow({ strategy: "oauth_google" });
+  // const handleGoogleSignIn = async () => {
+  //   try {
+  //     const { createdSessionId, setActive } = await startSSOFlow({ strategy: "oauth_google" });
 
-      if (setActive && createdSessionId) {
-        setActive({ session: createdSessionId });
-        router.replace("/(tabs)");
-      }
-    } catch (error) {
-      console.error("OAuth error:", error);
-    }
-  };
+  //     if (setActive && createdSessionId) {
+  //       setActive({ session: createdSessionId });
+  //       router.replace("/(tabs)");
+  //     }
+  //   } catch (error) {
+  //     console.error("OAuth error:", error);
+  //   }
+  // };
 
   return (
     <View style={styles.container}>
@@ -46,7 +46,8 @@ export default function login() {
       <View style={styles.loginSection}>
         <TouchableOpacity
           style={styles.googleButton}
-          onPress={handleGoogleSignIn}
+          // handleGoogleSignIn
+          onPress={() => console.log("google")}
           activeOpacity={0.9}
         >
           <View style={styles.googleIconContainer}>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,41 @@
-import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import InitialLayout from "@/components/InitialLayout";
+import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
+import { SplashScreen } from "expo-router";
+import { useFonts } from "expo-font";
+import { useCallback, useEffect } from "react";
+import * as NavigationBar from "expo-navigation-bar";
+import { Platform } from "react-native";
 
-import { ClerkProvider } from '@clerk/expo'
-import { tokenCache } from '@clerk/expo/token-cache'
-import { Slot } from 'expo-router'
+import { StatusBar } from "expo-status-bar";
 
-const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
-
-if (!publishableKey) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
-}
-
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  // update the native navigation bar on Android.
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync("#000000");
+      NavigationBar.setButtonStyleAsync("light");
+    }
+  }, []);
+
   return (
-    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+    <ClerkAndConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
-          <Stack screenOptions={{ headerShown: false }} />
+        <SafeAreaView style={{ flex: 1, backgroundColor: "#000" }} onLayout={onLayoutRootView}>
+          <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
-    </ClerkProvider>
+      <StatusBar style="light" />
+    </ClerkAndConvexProvider>
   );
 }
